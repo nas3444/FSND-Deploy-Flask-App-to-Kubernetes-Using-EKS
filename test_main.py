1886c66f96ed62cc98f9d2922203eacc22d887fd
nas3444 <nan@LAPTOP-I18RVVTM.localdomain>
2022-03-09 17:41:57 +0300

    finished

--- test_main.py
+++ test_main.py
@@ -24,8 +24,8 @@ def client():
 
 def test_health(client):
     response = client.get('/')
-    assert response.status_code == 404
-    assert response.json == 'UnHealthy'
+    assert response.status_code == 200
+    assert response.json == 'Healthy'
 
 
 def test_auth(client):
